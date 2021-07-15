d5e738e361128803258d2a2d1f0052060ab317b5
mario <support@kerbogames.com>
2021-07-15 01:39:45 -0700

    Refractor

--- Kernel/Kernel.h
+++ Kernel/Kernel.h
@@ -35,6 +35,15 @@ typedef U64                     UIntPtr64;
 #define I64_MIN    -0x8000000000000000
 #define I64_MAX     0x7FFFFFFFFFFFFFFF
 
+#define I8_C(_x)    _x
+#define U8_C(_x)    _x
+#define I16_C(_x)   _x
+#define U16_C(_x)   _x
+#define I32_C(_x)   _x
+#define U32_C(_x)   _x##U
+#define I64_C(_x)   _x##LL
+#define U64_C(_x)   _x##ULL
+
 #define TRUE        1
 #define FALSE       0
 #define NULL        ((void*)0)
@@ -134,41 +143,38 @@ enum Register {
 extern const char* const REG_NAMES[REGISTER_COUNT];
 
 /* Contains bit masks for all registers except for %eip! */
-enum RegisterMask {
-    REGISTER_MASK_RAX   = 1 << REGISTER_RAX,
-    REGISTER_MASK_RBX   = 1 << REGISTER_RBX,
-    REGISTER_MASK_RCX   = 1 << REGISTER_RCX,
-    REGISTER_MASK_RDX   = 1 << REGISTER_RDX,
-    REGISTER_MASK_RSI   = 1 << REGISTER_RSI,
-    REGISTER_MASK_RDI   = 1 << REGISTER_RDI,
-    REGISTER_MASK_RSP   = 1 << REGISTER_RSP,
-    REGISTER_MASK_RBP   = 1 << REGISTER_RBP,
-    REGISTER_MASK_R8    = 1 << REGISTER_R8 ,
-    REGISTER_MASK_R9    = 1 << REGISTER_R9 ,
-    REGISTER_MASK_R10   = 1 << REGISTER_R10,
-    REGISTER_MASK_R11   = 1 << REGISTER_R11,
-    REGISTER_MASK_R12   = 1 << REGISTER_R12,
-    REGISTER_MASK_R13   = 1 << REGISTER_R13,
-    REGISTER_MASK_R14   = 1 << REGISTER_R14,
-    REGISTER_MASK_R15   = 1 << REGISTER_R15,
-
-    REGISTER_MASK_XMM0  = 1 << REGISTER_XMM0,
-    REGISTER_MASK_XMM1  = 1 << REGISTER_XMM1,
-    REGISTER_MASK_XMM2  = 1 << REGISTER_XMM2,
-    REGISTER_MASK_XMM3  = 1 << REGISTER_XMM3,
-    REGISTER_MASK_XMM4  = 1 << REGISTER_XMM4,
-    REGISTER_MASK_XMM5  = 1 << REGISTER_XMM5,
-    REGISTER_MASK_XMM6  = 1 << REGISTER_XMM6,
-    REGISTER_MASK_XMM7  = 1 << REGISTER_XMM7,
-    REGISTER_MASK_XMM8  = 1 << REGISTER_XMM8,
-    REGISTER_MASK_XMM9  = 1 << REGISTER_XMM9,
-    REGISTER_MASK_XMM10 = 1 << REGISTER_XMM10,
-    REGISTER_MASK_XMM11 = 1 << REGISTER_XMM11,
-    REGISTER_MASK_XMM12 = 1 << REGISTER_XMM12,
-    REGISTER_MASK_XMM13 = 1 << REGISTER_XMM13,
-    REGISTER_MASK_XMM14 = 1 << REGISTER_XMM14,
-    REGISTER_MASK_XMM15 = 1 << REGISTER_XMM15,
-};
+#define REGISTER_MASK_RAX   UINT64_C(1) << REGISTER_RAX
+#define REGISTER_MASK_RBX   UINT64_C(1) << REGISTER_RBX
+#define REGISTER_MASK_RCX   UINT64_C(1) << REGISTER_RCX
+#define REGISTER_MASK_RDX   UINT64_C(1) << REGISTER_RDX
+#define REGISTER_MASK_RSI   UINT64_C(1) << REGISTER_RSI
+#define REGISTER_MASK_RDI   UINT64_C(1) << REGISTER_RDI
+#define REGISTER_MASK_RSP   UINT64_C(1) << REGISTER_RSP
+#define REGISTER_MASK_RBP   UINT64_C(1) << REGISTER_RBP
+#define REGISTER_MASK_R8    UINT64_C(1) << REGISTER_R8 
+#define REGISTER_MASK_R9    UINT64_C(1) << REGISTER_R9 
+#define REGISTER_MASK_R10   UINT64_C(1) << REGISTER_R10
+#define REGISTER_MASK_R11   UINT64_C(1) << REGISTER_R11
+#define REGISTER_MASK_R12   UINT64_C(1) << REGISTER_R12
+#define REGISTER_MASK_R13   UINT64_C(1) << REGISTER_R13
+#define REGISTER_MASK_R14   UINT64_C(1) << REGISTER_R14
+#define REGISTER_MASK_R15   UINT64_C(1) << REGISTER_R15
+#define REGISTER_MASK_XMM0  UINT64_C(1) << REGISTER_XMM0
+#define REGISTER_MASK_XMM1  UINT64_C(1) << REGISTER_XMM1
+#define REGISTER_MASK_XMM2  UINT64_C(1) << REGISTER_XMM2
+#define REGISTER_MASK_XMM3  UINT64_C(1) << REGISTER_XMM3
+#define REGISTER_MASK_XMM4  UINT64_C(1) << REGISTER_XMM4
+#define REGISTER_MASK_XMM5  UINT64_C(1) << REGISTER_XMM5
+#define REGISTER_MASK_XMM6  UINT64_C(1) << REGISTER_XMM6
+#define REGISTER_MASK_XMM7  UINT64_C(1) << REGISTER_XMM7
+#define REGISTER_MASK_XMM8  UINT64_C(1) << REGISTER_XMM8
+#define REGISTER_MASK_XMM9  UINT64_C(1) << REGISTER_XMM9
+#define REGISTER_MASK_XMM10 UINT64_C(1) << REGISTER_XMM10
+#define REGISTER_MASK_XMM11 UINT64_C(1) << REGISTER_XMM11
+#define REGISTER_MASK_XMM12 UINT64_C(1) << REGISTER_XMM12
+#define REGISTER_MASK_XMM13 UINT64_C(1) << REGISTER_XMM13
+#define REGISTER_MASK_XMM14 UINT64_C(1) << REGISTER_XMM14
+#define REGISTER_MASK_XMM15 UINT64_C(1) << REGISTER_XMM15
 
 /* Contians storage for all registers. */
 typedef union Register64 Register64AggregateSet[REGISTER_COUNT >> 1];
@@ -184,6 +190,13 @@ __attribute__((always_inline)) static inline union Register64 QueryRip() {
         : "=r"(rip)
     );
     return *(union Register64*)rip;
+}
+
+/* Read the value of the (TSC) time stamp counter. */
+__attribute__((always_inline)) static inline U64 ReadTsc() {
+    U32 lo, hi;
+    asm volatile("rdtsc" : "=a"(lo), "=d"(hi));
+    return (U64)lo | (U64)hi << 32;
 }
 
 /* Queries regset from register data. These values are not that accurate. */
--- Kernel/Startup.c
+++ Kernel/Startup.c
@@ -6,11 +6,6 @@ void KernelEntry(void) {
     ScreenPrint("Cyrax64 Microkernel (C) Copyright Mario Sieg \"pinsrq\" <mt3000@gmx.de>\n");
     ScreenPrint("64-bit long mode online!\n");
     ScreenPrint("Installing interrupt service routines...\n");
-    /*
-        IsrInstallation();
-        asm volatile("int3");
-        asm volatile("int $1");
-    */
 }
 
 void main(void) {
